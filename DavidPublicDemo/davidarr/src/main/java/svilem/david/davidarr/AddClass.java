a22e2296dc62bbc736dbf35ebd020ab90685f763
David Svilem <david@onoapps.com>
2015-08-17 11:51:36 +0300

    first commit

---
+++ DavidPublicDemo/davidarr/src/main/java/svilem/david/davidarr/AddClass.java
@@ -0,0 +1,12 @@
+package svilem.david.davidarr;
+
+/**
+ * Created by davidsvilem on 8/16/15.
+ */
+public class AddClass {
+
+    public static int add(int x, int y){
+        return x+y;
+    }
+
+}
